85eadeca92b27853abfeee17110e41f6f4aac1e1
Lia <liabifano@gmail.com>
2018-03-28 10:24:04 +0200

    pytorch dependency

--- bootstrap-python-env.sh
+++ bootstrap-python-env.sh
@@ -1,7 +1,8 @@
+#!/usr/bin/env bash
+
 ENVNAME=$(basename `pwd`)
 conda env remove -yq -n $ENVNAME &> /dev/null
 conda create -yq -n $ENVNAME --file conda.txt #1> /dev/null
 source activate $ENVNAME
+conda install pytorch torchvision -c pytorch
 pip install -e .
-
-# put pytorch!
