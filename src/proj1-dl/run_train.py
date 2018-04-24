92cc27dcc5d4ac9cd938142e3c57c217d925040b
Lia <liabifano@gmail.com>
2018-04-24 09:19:35 +0200

    first reasonable model

--- src/proj1-dl/run_train.py
+++ src/proj1-dl/run_train.py
@@ -1,7 +1,7 @@
 from sklearn import model_selection
 
 import dlc_bci as bci
-from networks import Net
+from networks import Conv_net
 from train import train_model_full
 
 MINI_BATCH_SIZE = 40
@@ -10,19 +10,25 @@ N_EPOCHS = 1000
 
 if __name__ == '__main__':
     train_input, train_target = bci.load(root='./data_bci')
-    X = train_input.numpy()
-    y = train_target.numpy()
     kfolds = model_selection.KFold(n_splits=N_FOLDS, random_state=1234, shuffle=True)
 
     test_input, test_target = bci.load(root='./data_bci', train=False)
 
     # put this inside the train to avoid data snooping
     mu, std = train_input.mean(0), train_input.std(0)
-    train_input.sub_(mu).div_(std)
-    test_input.sub_(mu).div_(std)
+    train_input = train_input.sub_(mu).div_(std)
+    test_input = test_input.sub_(mu).div_(std)
 
-    costs, costs_val, acc, acc_val = train_model_full(Net,
-                                                      {},
+    X = train_input.numpy()
+    y = train_target.numpy()
+
+    costs, costs_val, acc, acc_val = train_model_full(Conv_net,
+                                                      {   'kernel_size': [4],
+                                                          'layers': [40, 2],
+                                                          'layers_conv': [28, 15],
+                                                          'p': [0.10799015707321313, 0.24230368007875924],
+                                                          'pooling_kernel_size': [2],
+                                                          'size': 50},
                                                       X,
                                                       y,
                                                       MINI_BATCH_SIZE,
